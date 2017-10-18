4d0c78527a10f6341ca12276e7b323724c3d8d4d
Esteban r <esteban.ramos.f@gmail.com>
2017-10-18 03:59:43 -0300

    update/add - add method to edit data in filemaker with curl.

--- app/Http/Controllers/FileMakerApiRestController.php
+++ app/Http/Controllers/FileMakerApiRestController.php
@@ -330,7 +330,7 @@ class FileMakerApiRestController extends Controller
 
 
 
-   function callCURL ($url, $method, $payload='', $token) {
+   function callCURLFind ($url, $method, $payload='', $token) {
       if (is_array ($payload)) $payload = json_encode ($payload);
 
       $ch = curl_init();
@@ -344,7 +344,7 @@ class FileMakerApiRestController extends Controller
       $query = \GuzzleHttp\json_encode([
          'query' => [['Us_Usuario' => '=Victor', 'Us_pass' => '=123']]
       ]);
-      
+
       if (!$token) {
          curl_setopt ($ch, CURLOPT_HTTPHEADER, array ('FM-Data-token:'. $token , 'Content-Type:application/json'));
       } else
@@ -369,6 +369,41 @@ class FileMakerApiRestController extends Controller
       return json_decode($result, true);
    }
 
+   function callCURLEdit ($url, $method, $payload='', $token) {
+      if (is_array ($payload)) $payload = json_encode ($payload);
+
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);         //follow redirects
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);         //return the transfer as a string
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);         //don't verify SSL CERT
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);         //don't verify SSL CERT
+      curl_setopt($ch, CURLOPT_VERBOSE, true);
+      curl_setopt($ch, CURLOPT_FRESH_CONNECT, TRUE);       //Don'T use cache
+
+      if (!$token) {
+         curl_setopt ($ch, CURLOPT_HTTPHEADER, array ('FM-Data-token:'. $token , 'Content-Type:application/json'));
+      } else
+      {
+         curl_setopt ($ch, CURLOPT_HTTPHEADER, array ('FM-Data-token:'. $token , 'Content-Type:application/json'));
+      }
+      if (!empty ($payload)) {
+         if ($method == 'GET') {
+            $url = $url . '?' . $payload;
+         } else {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $payload );
+         }
+      }
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+      curl_setopt($ch, CURLOPT_URL, $url);
+      $result = curl_exec($ch);
+      $error = curl_error ($ch);
+      $info = curl_getinfo ($ch);
+      curl_close($ch);
+
+      dd($result);
+      return json_decode($result, true);
+   }
+
 
    public function test_show (Request $request) {
       try {
@@ -383,18 +418,40 @@ class FileMakerApiRestController extends Controller
          #return response()->json($responseContents);
          #Solicitar datos con el login (concatena el layout a consultar)
          $post_uri = 'https://201.238.235.30/fmi/rest/api/find/'.rawurlencode('Tasks_FMAngular').'/'.rawurlencode('usuarios');
+         $edit_uri = 'https://201.238.235.30/fmi/rest/api/record/'.rawurlencode('Tasks_FMAngular').'/'.rawurlencode('usuarios').'/2';
 
+         #/fmi/rest/api/record/:solution/:layout/:recordId
          #Configura headers para hacer la peticion + token
 
+         /*
          $payload = [
             'user' => 'nuevo',
             'password' => '1234',
             'layout' => 'usuarios',
-         ] ;
+         ];
+         */
+
+         $body = \GuzzleHttp\json_encode([
+            'data' => [
+               'Us_Nombre' => 'Vitoco',
+               'Us_Apellido_P' => 'Garrafa',
+               'Us_Apellido_M' => 'Sep.'
+            ],
+            'modId' => '3'
+         ]);
+
+         $_body = [
+            'data' => [
+               'Us_Nombre' => 'Vitoco',
+               'Us_Apellido_P' => 'Garrafa',
+               'Us_Apellido_M' => 'Sep.'
+            ],
+         ];
 
 
 
-         $this->callCURL($post_uri, 'POST', $payload, $responseContents->token);
+         #$this->callCURLFind($post_uri, 'POST', $payload, $responseContents->token);
+         $this->callCURLEdit($edit_uri, 'PUT', $body, $responseContents->token);
 
 
          dd('');
